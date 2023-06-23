ff7f91a3bf5d7ed45f48d5a6a4a79180a86dd249
Henrique <hecaetano1@hotmail.com>
2023-06-22 21:13:02 -0300

    :recycle: refactoring

--- main.c
+++ main.c
@@ -5,35 +5,33 @@
 #include <limits.h>
 
 #include "operation.h"
+#include "services.h"
 
 int main(int argc, char const *argv[]){
     system("clear");
 
-    char* line = NULL; size_t len = 0;
+    char* input;
 
     char currentDirectory[1000];
     getcwd(currentDirectory, sizeof(currentDirectory)); 
 
     while (1){
-        printf("acsh %s > ", currentDirectory);
-        int tam = getline(&line, &len, stdin);
-        line[tam-1] = '\0'; // TODO: CRIME CRIME ALO CRIME
+        shellHeader(currentDirectory);
 
-        char* command = strtok(line, " "); 
+        //get user input
+        input = inputEntry();
+
+        char* command = strtok(input, " ");
         char* args = strtok(NULL, " ");
 
-        // acho que vale a pena tirar da main rs
-        if(getOperation(command) == 1){
-            changeDirectory(command, args, currentDirectory);
+        //find out which type of task it is
+        int taskType = taskCaseHandler(command);
 
-        }else if(getOperation(command) == 2){
-            printf("%s\n", line);
-            exitShell(command);
+        //actually executes the task
+        taskPerform(taskType, command, args, currentDirectory);
 
-        }
-
-        free(line);
-        char* line = NULL; size_t len = 0;
+        //pre requisite for inputEntry function
+        free(input);
     }
     
     return 0;
--- makefile
+++ makefile
@@ -1,16 +1,22 @@
-acsh: main.o operation.o
-	@gcc -o acsh main.o operation.o
+acsh: main.o operation.o services.o
+	@gcc -o acsh main.o operation.o services.o
 
 
-main.o: main.c operation.h
+main.o: main.c operation.h services.h
 	@gcc -o main.o main.c -c
 
 
 operation.o: operation.c operation.h
 	@gcc -o operation.o operation.c -c 
 
+services.o: services.c services.h
+	@gcc -o services.o services.c -c
+
 clean:
 	@rm -rf *.o
 
 run: acsh
 	@./acsh
+
+val: acsh
+	@valgrind ./acsh
