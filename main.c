207de05ab0abe63cd67e3c1df73d27d74cfba596
thamyadonadia <thamya.donadia@edu.ufes.br>
2023-06-23 13:01:01 -0300

    :bug: fixed input problems

--- main.c
+++ main.c
@@ -10,7 +10,9 @@
 int main(int argc, char const *argv[]){
     system("clear");
     
-    char* input, cwd = malloc(sizeof(char)*1000);
+    char* input; char* cwd = malloc(sizeof(char)*1000);
+    char* inputCopy;
+    int countLeaders = 0; int sizeSessionLeaders = 10000; int* sessionLeaders = malloc(sizeof(int)*sizeSessionLeaders);
     getcwd(cwd, sizeof(cwd)); 
 
     while (1){
@@ -18,17 +20,19 @@ int main(int argc, char const *argv[]){
 
         //get user input
         input = inputEntry();
-
+        inputCopy = strdup(input);
         //find out which type of task it is
         int taskType = taskCaseHandler(input);
 
         //actually executes the task
-        taskPerform(taskType, input, cwd);
+        taskPerform(taskType, inputCopy, cwd, sessionLeaders, &countLeaders, &sizeSessionLeaders);
 
         //pre requisite for inputEntry function
         free(input);
+        free(inputCopy);
     }
     
     free(cwd);
+    free(sessionLeaders);
     return 0;
 }
