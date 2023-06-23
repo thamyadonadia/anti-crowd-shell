8f6ecb18952a0483896f49f2cc7039d65470949d
thamyadonadia <thamya.donadia@edu.ufes.br>
2023-06-23 10:57:51 -0300

    :sparkles: add exit, background and foreground functions

--- operation.c
+++ operation.c
@@ -3,16 +3,88 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
+// ========== Operações Internas ============
 void changeDirectory(char* path, char* cwd){
     chdir(path);
     //printf("%s\n",getcwd(cwd, 1000));
     getcwd(cwd, 1000);
 }
 
-void exitShell(){
-    printf("opa, vc está querendo sair do shell?\n");
+void exitShell(char* input, int* sessionLeaders, int countLeaders){
+    for(int i=0; i<countLeaders; i++){
+        kill(sessionLeaders[i], SIGKILL);
+    }
+
+    free(sessionLeaders); free(input);
+    exit(0);
+}
+
+
+// =========== Execução de programas ==========
+void foregroungProcess(char* command){ // recebe o comando sem o %
+    // nome do executável + no máximo 3 argumentos
+    pid_t pid = fork();
+    char* filename = strtok(command, ' '); char* args[3]; int i=0;
+
+    printf("filename: %s\n", filename);
+
+    while(args[i] && i<2){
+        args[i] = strtok(NULL, ' '); 
+        printf("arg[%d] = %s\n", i, args[i]);
+    }
+
+
+
+    if(!pid) execvp(filename, args); // no filho
+    if(pid) waitpid(pid, NULL, WUNTRACED);
+}
+
+int backgroundGroupProcess(char* input){
+    int len = strlen(input);
+    int processCount=1; 
+    pid_t leader; pid_t processGroupID;
+
+    for(int i=0; i<len; i++){
+        //possivel delimitador encontrado
+        if(input[i]=='<' && input[i+1]=='3'){
+            //confirmação da delimitação e que há outro programa a ser executado
+            if(input[i-1]== ' ' && input[i+2]== ' ') processCount++;
+        }
+    }
+
+    printf("number of process = %d\n", processCount);
+
+    /*for(int j=0; j<processCount; j++){
+        char* filename = strtok(input, ' '); char* args[3]; int i=0;
+
+        while(args[i] && i<2){
+            args[i] = strtok(NULL, ' '); 
+        }
+        
+        if(j==0){ 
+            leader = fork();
+
+            if(!leader){
+                processGroupID = setsid();
+                execvp(filename, args);
+            }
+            
+        }else{
+            pid_t pid = fork();
+
+            if(!pid){
+                setpgid(pid, processGroupID);
+                execvp(filename, args);
+            }
+        }
+
+    }*/
+
+    return leader;
 }
 
 void list(char* args){
--- operation.h
+++ operation.h
@@ -1,11 +1,25 @@
 #ifndef OPERATION_H
 #define OPERATION_H
 
-
+/** Altera o diretório corrente do shell
+ * @param char* caminho do diretório destino
+ * @param char* diretório atual do shell
+*/
 void changeDirectory(char* path, char* cwd);
 
 //must free everything
-void exitShell();
+void exitShell(char* input, int* sessionLeaders, int countLeaders);
+
+/** Executa um comando em foreground
+ * @param char* comando a ser executado em foreground, sem o operador especial
+*/
+void foregroungProcess(char* command);
+
+/** Executa até 5 comandos em background
+ * @param char* input passado na linha de comando
+ * @return int id do processo líder da sessão
+*/
+int backgroundGroupProcess(char* input);
 
 void list(char* args);
 
